2ab26e3655bead7938584aae083c328d131f26b4
hongminhbka <hongminhbka@gmail.com>
2023-01-03 16:37:23 +0700

    edit product shop

--- wp-content/themes/vizeon/woocommerce/archive-product.php
+++ wp-content/themes/vizeon/woocommerce/archive-product.php
@@ -83,27 +83,132 @@ if ( is_shop() || is_product_category() || is_product_tag() ) {
             <h1 class="page-title hidden"><?php woocommerce_page_title(); ?></h1>
 
           <?php endif; ?>
-
+          <section class="elementor-element elementor-section-full_width elementor-section-height-default elementor-section-height-default elementor-section elementor-top-section">
+      <div class="elementor-container elementor-column-gap-default">
+        <div class="elementor-row">
+          <div class="elementor-element elementor-column elementor-col-100 elementor-top-column">
+            <div class="elementor-column-wrap elementor-element-populated">
+              <div class="elementor-widget-wrap">
+                <section class="elementor-element elementor-section-boxed elementor-section-height-default elementor-section-height-default elementor-section elementor-inner-section">
+                  <div class="elementor-container elementor-column-gap-default">
+                    <div class="elementor-row">
+                      <div class="elementor-element elementor-column elementor-col-100 elementor-inner-column">
+                        <div class="elementor-column-wrap elementor-element-populated">
+                          <div class="elementor-widget-wrap">
+                            <div class="elementor-element elementor-widget elementor-widget-gva-heading-block">
+                              <div class="elementor-widget-container">
+                                <div class="gva-element-gva-heading-block gva-element">
+                                  <div class="align-left style-1 widget gsc-heading">
+                                    <div class="content-inner">
+                                      <div class="title">Bạn đang tìm sản phẩm cho xe gì</div>
+                                      <div class="title-desc">Raijin có đủ sản phẩm cho tất cả các dòng xe</div>
+                                    </div>
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </section>
+                <section class="elementor-element elementor-element-bcad63e elementor-section-boxed elementor-section-height-default elementor-section-height-default elementor-section elementor-inner-section">
+                  <div class="elementor-container elementor-column-gap-default">
+                    <div class="elementor-row">
+                      <div class="elementor-element elementor-column elementor-col-25 elementor-inner-column">
+                        <div class="elementor-column-wrap elementor-element-populated">
+                          <div class="elementor-widget-wrap">
+                            <div class="elementor-element elementor-align-center elementor-widget elementor-widget-button">
+                              <div class="elementor-widget-container">
+                                <div class="elementor-button-wrapper">
+                                  <a href="/danh-muc-san-pham/honda/" class="elementor-button-link elementor-button elementor-size-md full-width">
+                                    <span class="elementor-button-content-wrapper">
+                                      <span class="elementor-button-text">Honda</span>
+                                    </span>
+                                  </a>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                      <div class="elementor-element elementor-column elementor-col-25 elementor-inner-column">
+                        <div class="elementor-column-wrap  elementor-element-populated">
+                          <div class="elementor-widget-wrap">
+                            <div class="elementor-element elementor-align-center elementor-widget elementor-widget-button">
+                              <div class="elementor-widget-container">
+                                <div class="elementor-button-wrapper">
+                                  <a href="/danh-muc-san-pham/yamaha/" class="elementor-button-link elementor-button elementor-size-md">
+                                    <span class="elementor-button-content-wrapper">
+                                      <span class="elementor-button-text">Yamaha</span>
+                                    </span>
+                                  </a>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                      <div class="elementor-element elementor-column elementor-col-25 elementor-inner-column">
+                        <div class="elementor-column-wrap elementor-element-populated">
+                          <div class="elementor-widget-wrap">
+                            <div class="elementor-element elementor-align-center elementor-widget elementor-widget-button">
+                              <div class="elementor-widget-container">
+                                <div class="elementor-button-wrapper">
+                                  <a href="/danh-muc-san-pham/piaggio/"
+                                    class="elementor-button-link elementor-button elementor-size-md">
+                                    <span class="elementor-button-content-wrapper">
+                                      <span class="elementor-button-text">Piaggio</span>
+                                    </span>
+                                  </a>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                      <div class="elementor-element elementor-column elementor-col-25 elementor-inner-column">
+                        <div class="elementor-column-wrap elementor-element-populated">
+                          <div class="elementor-widget-wrap">
+                            <div class="elementor-element elementor-align-center elementor-widget elementor-widget-button">
+                              <div class="elementor-widget-container">
+                                <div class="elementor-button-wrapper">
+                                  <a href="/danh-muc-san-pham/san-pham-khac/" class="elementor-button-link elementor-button elementor-size-md">
+                                    <span class="elementor-button-content-wrapper">
+                                      <span class="elementor-button-text">Xe hãng khác</span>
+                                    </span>
+                                  </a>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </section>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
           <?php do_action('woocommerce_archive_description'); ?>          
-          <?php
-            // Run the loop
-           if ( $products->have_posts() ) :
-            do_action( 'woocommerce_before_shop_loop' );
-            woocommerce_product_loop_start();
-            woocommerce_product_subcategories();
-
-            while ( $products->have_posts() ) : $products->the_post();
-              do_action( 'woocommerce_shop_loop' );
-              wc_get_template_part( 'content', 'product' );
-            endwhile;
-
-            woocommerce_product_loop_end();
-            do_action( 'woocommerce_after_shop_loop' );
-            wp_reset_postdata();
-          elseif ( ! woocommerce_product_subcategories( array( 'before' => woocommerce_product_loop_start( false ), 'after' => woocommerce_product_loop_end( false ) ) ) ) :
-            do_action( 'woocommerce_no_products_found' );
-          endif;
-          ?>
+          <?php if ( $products->have_posts()) :?>
+            <?php do_action( 'woocommerce_before_shop_loop' ); ?>
+            <?php woocommerce_product_loop_start(); ?>
+            <?php woocommerce_product_subcategories();?>
+            <?php while ($products->have_posts()) : the_post(); ?>
+              <?php do_action('woocommerce_shop_loop')?>
+              <?php wc_get_template_part('content', 'product'); ?>
+            <?php endwhile;?>
+            <?php woocommerce_product_loop_end(); ?>
+            <?php do_action( 'woocommerce_after_shop_loop' );?>
+            <?php wp_reset_postdata();?>
+          <?php elseif (!woocommerce_product_subcategories(array('before' => woocommerce_product_loop_start(false), 'after' => woocommerce_product_loop_end(false)))) : ?>
+            <?php wc_get_template('loop/no-products-found.php'); ?>
+          <?php endif; ?>
         </div>
 
       </div>
