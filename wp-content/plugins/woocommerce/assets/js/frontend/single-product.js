187fc6b74f88d64152e16db933fa694b867df5ae
hongminhbka <hongminhbka@gmail.com>
2023-01-03 10:10:52 +0700

    remote icon preview image product

--- wp-content/plugins/woocommerce/assets/js/frontend/single-product.js
+++ wp-content/plugins/woocommerce/assets/js/frontend/single-product.js
@@ -236,7 +236,7 @@ jQuery( function( $ ) {
 	 */
 	ProductGallery.prototype.initPhotoswipe = function() {
 		if ( this.zoom_enabled && this.$images.length > 0 ) {
-			this.$target.prepend( '<a href="#" class="woocommerce-product-gallery__trigger">🔍</a>' );
+			this.$target.prepend( '<a href="#" class="woocommerce-product-gallery__trigger"></a>' );
 			this.$target.on( 'click', '.woocommerce-product-gallery__trigger', this.openPhotoswipe );
 			this.$target.on( 'click', '.woocommerce-product-gallery__image a', function( e ) {
 				e.preventDefault();
