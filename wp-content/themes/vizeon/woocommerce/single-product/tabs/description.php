e8cd8d1194e609bd8c6e54c9d6c688aef3941175
tongdv <hongminhbka@gmail.com>
2023-01-06 23:07:20 +0700

    edit title

--- wp-content/themes/vizeon/woocommerce/single-product/tabs/description.php
+++ wp-content/themes/vizeon/woocommerce/single-product/tabs/description.php
@@ -11,11 +11,10 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
 global $post;
 
-$heading = esc_html( apply_filters( 'woocommerce_product_description_heading', esc_html__( 'Product Description', 'vizeon' ) ) );
 ?>
 
 <?php if ( $heading ): ?>
-  <h2 class="tab-title hidden"><?php echo esc_html($heading); ?></h2>
+  <h2 class="tab-title hidden">Mô tả sản phẩm</h2>
 <?php endif; ?>
 
 <?php the_content(); ?>
