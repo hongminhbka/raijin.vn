941ef2545375da30ea3d99bdf4f0693e41db6128
tongdv <hongminhbka@gmail.com>
2023-01-07 15:42:48 +0700

    edit

--- wp-content/themes/vizeon/woocommerce/single-product/related.php
+++ wp-content/themes/vizeon/woocommerce/single-product/related.php
@@ -359,7 +359,7 @@ $woocommerce_loop['columns'] = $columns;
 										<div class="lg-block-grid-3 md-block-grid-3 sm-block-grid-2 xs-block-grid-1">
 											<?php while ($posts->have_posts()) : $posts->the_post(); ?>
 												<?php echo '<div class="item-columns margin-bottom-30">';?>
-                  									<?php get_template_part( 'templates/content/item', 'post-style-small' );?>
+                  									<?php get_template_part( 'templates/content/item', 'post-style-1' );?>
                 								<?php echo '</div>';?>
 											<?php endwhile;?>
 										</div>
