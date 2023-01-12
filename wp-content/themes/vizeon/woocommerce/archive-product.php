b6fd29a1d0a5434729b8b187ef8b272a1cc1e5fb
hongminhbka <hongminhbka@gmail.com>
2023-01-12 17:16:58 +0700

    remove

--- wp-content/themes/vizeon/woocommerce/archive-product.php
+++ wp-content/themes/vizeon/woocommerce/archive-product.php
@@ -116,7 +116,7 @@ $woo_display = vizeon_display_modes_value();
                                     );
                                 
                                     foreach ( get_terms( $args_query ) as $key => $term ) {
-                                      if( $term->slug != 'ac-quy-lithium-xe-may' && $key < 3 ) {
+                                      if( $key < 3 ) {
                                         echo '<div class="elementor-element elementor-column elementor-col-25 elementor-inner-column">
                                               <div class="elementor-column-wrap elementor-element-populated">
                                                 <div class="elementor-widget-wrap">
