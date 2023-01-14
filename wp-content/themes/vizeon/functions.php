35d8bb6774809a9c1a6ec7e2dd906d00d96ebcc8
tongdv <hongminhbka@gmail.com>
2023-01-14 15:44:11 +0700

    edit title

--- wp-content/themes/vizeon/functions.php
+++ wp-content/themes/vizeon/functions.php
@@ -487,21 +487,20 @@ add_shortcode ('woo_cart_but', 'woo_cart_but' );
  */
 function woo_cart_but() {
 	ob_start();
-        $cart_count = WC()->cart->cart_contents_count; // Set variable for cart item count
-        $cart_url = wc_get_cart_url();  // Set Cart URL
-  
-        ?>
-        <li><a class="menu-item cart-contents" href="<?php echo $cart_url; ?>" title="My Basket">
-	    <?php
-        if ( $cart_count > 0 ) {
-       ?>
-            <span class="cart-contents-count"><?php echo $cart_count; ?></span>
-        <?php
-        }
-        ?>
-        </a></li>
-        <?php
-    return ob_get_clean();
+  $cart_count = WC()->cart->cart_contents_count;
+  $cart_url = wc_get_cart_url();  
+  ?>
+  <li><a class="menu-item cart-contents" href="<?php echo $cart_url; ?>" title="Giỏ hàng">
+<?php
+  if ( $cart_count > 0 ) {
+  ?>
+      <span class="cart-contents-count"><?php echo $cart_count; ?></span>
+  <?php
+  }
+  ?>
+  </a></li>
+  <?php
+  return ob_get_clean();
 }
 
 add_filter( 'woocommerce_add_to_cart_fragments', 'woo_cart_but_count' );
@@ -516,8 +515,8 @@ function woo_cart_but_count( $fragments ) {
     $cart_url = wc_get_cart_url();
     
     ?>
-    <a class="cart-contents menu-item" href="<?php echo $cart_url; ?>" title="<?php _e( 'View your shopping cart' ); ?>">
-	<?php
+    <a class="cart-contents menu-item" href="<?php echo $cart_url; ?>" title="<?php _e( 'Xem giỏ hàng' ); ?>">
+	  <?php
     if ( $cart_count > 0 ) {
         ?>
         <span class="cart-contents-count"><?php echo $cart_count; ?></span>
