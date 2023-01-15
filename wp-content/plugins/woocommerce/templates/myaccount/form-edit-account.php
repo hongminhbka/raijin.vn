6c6a70d1803b2ab9126cab1a3bd0715556a9fb13
tongdv <hongminhbka@gmail.com>
2023-01-15 16:08:28 +0700

    edit

--- wp-content/plugins/woocommerce/templates/myaccount/form-edit-account.php
+++ wp-content/plugins/woocommerce/templates/myaccount/form-edit-account.php
@@ -35,7 +35,7 @@ do_action( 'woocommerce_before_edit_account_form' ); ?>
 
 	<p class="woocommerce-form-row woocommerce-form-row--wide form-row form-row-wide">
 		<label for="account_display_name">Tên hiển thị&nbsp;<span class="required">*</span></label>
-		<input type="text" class="woocommerce-Input woocommerce-Input--text input-text" name="account_display_name" id="account_display_name" value="<?php echo esc_attr( $user->display_name ); ?>" /> <span><em><?php esc_html_e( 'This will be how your name will be displayed in the account section and in reviews', 'woocommerce' ); ?></em></span>
+		<input type="text" class="woocommerce-Input woocommerce-Input--text input-text" name="account_display_name" id="account_display_name" value="<?php echo esc_attr( $user->display_name ); ?>" /> <span><em>Đây sẽ là cách tên của bạn sẽ được hiển thị trong phần tài khoản và trong các bài đánh giá</em></span>
 	</p>
 	<div class="clear"></div>
 
