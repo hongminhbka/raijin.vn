96f42a787ae5e55eb4f1609b66129bd709bcc616
tongdv <hongminhbka@gmail.com>
2023-01-15 15:43:11 +0700

    edit

--- wp-content/plugins/woocommerce/templates/myaccount/downloads.php
+++ wp-content/plugins/woocommerce/templates/myaccount/downloads.php
@@ -37,9 +37,9 @@ do_action( 'woocommerce_before_account_downloads', $has_downloads ); ?>
 <?php else : ?>
 	<div class="woocommerce-Message woocommerce-Message--info woocommerce-info">
 		<a class="woocommerce-Button button" href="<?php echo esc_url( apply_filters( 'woocommerce_return_to_shop_redirect', wc_get_page_permalink( 'shop' ) ) ); ?>">
-			<?php esc_html_e( 'Browse products', 'woocommerce' ); ?>
+			Xem sản phẩm
 		</a>
-		<?php esc_html_e( 'No downloads available yet.', 'woocommerce' ); ?>
+		<?php esc_html_e( 'Không có bản tải xuống nào.', 'woocommerce' ); ?>
 	</div>
 <?php endif; ?>
 
