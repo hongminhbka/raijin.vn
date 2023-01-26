44ba2b9e933668b9975c60364e51a4109bdfc2a6
tongdv <hongminhbka@gmail.com>
2023-01-26 13:40:54 +0700

    fix null

--- wp-content/themes/vizeon/woocommerce/archive-product.php
+++ wp-content/themes/vizeon/woocommerce/archive-product.php
@@ -790,7 +790,7 @@ $woo_display = vizeon_display_modes_value();
                           }
                           $shortcode = '[products category="xe-may" attribute="phan-khuc" terms="'. $term->slug .'"]';
                           $output = do_shortcode($shortcode);
-                          if($output != '') {
+                          if($output != '<div class="woocommerce columns-3 "></div>') {
                             echo '<p class="elementor-heading-title elementor-size-default text-center">Ắc quy Lithium xe máy ' . $term->name . '</p>';
                             echo $output;
                           }
@@ -805,7 +805,7 @@ $woo_display = vizeon_display_modes_value();
                           }
                           $shortcode = '[products category="ac-quy-pkl" attribute="phan-khuc" terms="'. $term->slug .'"]';
                           $output = do_shortcode($shortcode);
-                          if($output != '') {
+                          if($output != '<div class="woocommerce columns-3 "></div>') {
                             echo '<p class="elementor-heading-title elementor-size-default text-center">Ắc quy Lithium xe mô tô ' . $term->name . '</p>';
                             echo $output;
                           }
@@ -820,7 +820,7 @@ $woo_display = vizeon_display_modes_value();
                           }
                           $shortcode = '[products category="o-to" attribute="phan-khuc" terms="'. $term->slug .'"]';
                           $output = do_shortcode($shortcode);
-                          if($output != '') {
+                          if($output != '<div class="woocommerce columns-3 "></div>') {
                             echo '<p class="elementor-heading-title elementor-size-default text-center">Ắc quy Lithium xe ô tô ' . $term->name . '</p>';
                             echo $output;
                           }
@@ -835,7 +835,7 @@ $woo_display = vizeon_display_modes_value();
                           }
                           $shortcode = '[products category="xe-may-dien" attribute="phan-khuc" terms="'. $term->slug .'"]' ;
                           $output = do_shortcode($shortcode);
-                          if($output != '') {
+                          if($output != '<div class="woocommerce columns-3 "></div>') {
                             echo '<p class="elementor-heading-title elementor-size-default text-center">Ắc quy Lithium xe máy điện ' . $term->name . '</p>';
                             echo $output;
                           }
