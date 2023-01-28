3a7ea68c0c4b233789cdf6d52aebf51b78dffd0b
tongdv <hongminhbka@gmail.com>
2023-01-28 09:36:45 +0700

    edit header

--- wp-content/themes/vizeon/header-builder.php
+++ wp-content/themes/vizeon/header-builder.php
@@ -23,6 +23,7 @@
   <meta http-equiv="content-type" content="text/html; charset=<?php bloginfo( 'charset' ); ?>">
   <meta name="apple-touch-fullscreen" content="yes"/>
   <meta name="MobileOptimized" content="320"/>
+  <meta property=”og:image:alt” content="Ắc quy Raijin">
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
   <link rel="profile" href="http://gmpg.org/xfn/11">
   <script>!function(e){"function"==typeof define&&define.amd?define(e):e()}(function(){var e,t=["scroll","wheel","touchstart","touchmove","touchenter","touchend","touchleave","mouseout","mouseleave","mouseup","mousedown","mousemove","mouseenter","mousewheel","mouseover"];if(function(){var e=!1;try{var t=Object.defineProperty({},"passive",{get:function(){e=!0}});window.addEventListener("test",null,t),window.removeEventListener("test",null,t)}catch(e){}return e}()){var n=EventTarget.prototype.addEventListener;e=n,EventTarget.prototype.addEventListener=function(n,o,r){var i,s="object"==typeof r&&null!==r,u=s?r.capture:r;(r=s?function(e){var t=Object.getOwnPropertyDescriptor(e,"passive");return t&&!0!==t.writable&&void 0===t.set?Object.assign({},e):e}(r):{}).passive=void 0!==(i=r.passive)?i:-1!==t.indexOf(n)&&!0,r.capture=void 0!==u&&u,e.call(this,n,o,r)},EventTarget.prototype.addEventListener._original=e}});</script>
