cf8ce6c8b5f8a2b5619020be343320453e2e46e6
tongdv <hongminhbka@gmail.com>
2023-01-29 09:32:20 +0700

    add content

--- wp-content/themes/vizeon/functions.php
+++ wp-content/themes/vizeon/functions.php
@@ -113,6 +113,60 @@ function vizeon_widgets_init() {
         'before_title' => '<h3 class="widget-title"><span>',
         'after_title' => '</span></h3>',
     ) );
+    register_sidebar( array(
+        'name' => esc_html__( 'Blog Sidebar', 'vizeon' ),
+        'id' => 'blog_sidebar',
+        'description' => esc_html__( 'Appears in the Blog sidebar section of the site.', 'vizeon' ),
+        'before_widget' => '<aside id="%1$s" class="widget clearfix %2$s">',
+        'after_widget' => '</aside>',
+        'before_title' => '<h3 class="widget-title"><span>',
+        'after_title' => '</span></h3>',
+    ) );
+    register_sidebar( array(
+        'name' => esc_html__( 'Page Sidebar', 'vizeon' ),
+        'id' => 'other_sidebar',
+        'description' => esc_html__( 'Appears in the Page Sidebar section of the site.', 'vizeon' ),
+        'before_widget' => '<aside id="%1$s" class="widget clearfix %2$s">',
+        'after_widget' => '</aside>',
+        'before_title' => '<h3 class="widget-title"><span>',
+        'after_title' => '</span></h3>',
+    ) );
+    register_sidebar( array(
+        'name' => esc_html__( 'Footer first', 'vizeon' ),
+        'id' => 'footer-sidebar-1',
+        'description' => esc_html__( 'Appears in the Footer first section of the site.', 'vizeon' ),
+        'before_widget' => '<aside id="%1$s" class="widget clearfix %2$s">',
+        'after_widget' => '</aside>',
+        'before_title' => '<h3 class="widget-title"><span>',
+        'after_title' => '</span></h3>',
+    ) );
+    register_sidebar( array(
+        'name' => esc_html__( 'Footer second', 'vizeon' ),
+        'id' => 'footer-sidebar-2',
+        'description' => esc_html__( 'Appears in the Footer second section of the site.', 'vizeon' ),
+        'before_widget' => '<aside id="%1$s" class="widget clearfix %2$s">',
+        'after_widget' => '</aside>',
+        'before_title' => '<h3 class="widget-title"><span>',
+        'after_title' => '</span></h3>',
+    ) );
+    register_sidebar( array(
+        'name' => esc_html__( 'Footer third', 'vizeon' ),
+        'id' => 'footer-sidebar-3',
+        'description' => esc_html__( 'Appears in the Footer third section of the site.', 'vizeon' ),
+        'before_widget' => '<aside id="%1$s" class="widget clearfix %2$s">',
+        'after_widget' => '</aside>',
+        'before_title' => '<h3 class="widget-title"><span>',
+        'after_title' => '</span></h3>',
+    ) );
+    register_sidebar( array(
+        'name' => esc_html__( 'Footer four', 'vizeon' ),
+        'id' => 'footer-sidebar-4',
+        'description' => esc_html__( 'Appears in the Footer four section of the site.', 'vizeon' ),
+        'before_widget' => '<aside id="%1$s" class="widget clearfix %2$s">',
+        'after_widget' => '</aside>',
+        'before_title' => '<h3 class="widget-title"><span>',
+        'after_title' => '</span></h3>',
+    ) );
 }
 add_action( 'widgets_init', 'vizeon_widgets_init' );
 
