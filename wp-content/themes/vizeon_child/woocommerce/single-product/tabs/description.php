be993531d7b57a7ef9d6b557f7032afa9794fb4d
Quoc Thang <thangpq.hust@gmail.com>
2023-03-23 11:02:03 +0700

    add Xem thêm

--- wp-content/themes/vizeon_child/woocommerce/single-product/tabs/description.php
+++ wp-content/themes/vizeon_child/woocommerce/single-product/tabs/description.php
@@ -12,5 +12,20 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 global $post;
 
 ?>
+<script>
+const readMoreBtn = document.querySelector(".read-more-btn");
+const text = document.querySelector(".mtext");
 
+readMoreBtn.addEventListener("click", (e) => {
+  text.classList.toggle("show-more");
+  if (readMoreBtn.innerText === "Read More") {
+    readMoreBtn.innerText = "Read Less";
+  } else {
+    readMoreBtn.innerText = "Read More";
+  }
+});
+</script>
+<div class="mtext"><span class="dots"> ...</span> <span class="moreText">
 <?php the_content(); ?>
+</span></div>
+<button class="read-more-btn">Read More</button>
